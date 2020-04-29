7c3de87d316cd13791667f0334e972c48154a020
codebrix <codebrix@gmail.com>
2020-04-29 22:21:36 +0300

    09.01.2020

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, jsonify, request
 import os.path
+import datetime
 
 # from livereload import Server
 
 app = Flask(__name__)
 
 app.debug = True
+app.config['JSON_AS_ASCII'] = False
 
 routes = {
     "home": {
@@ -30,6 +32,14 @@ routes = {
         'nav_title': 'Valstis',
         'template': 'countries.html',
         'ico': 'js/valstis/img/globe-earth.png'
+    },
+    "chat": {
+        'id': 3,
+        'route': '/chat',
+        'page_title': "Čats - Manas spēles",
+        'nav_title': 'Čats',
+        'template': 'chat.html',
+        'ico': 'js/chat/img/chat.png'
     }
 }
 
@@ -49,6 +59,37 @@ def countries():
     return index(routes['countries'])
 
 
+# chat
+@app.route(routes['chat']['route'])
+def chat():
+    return index(routes['chat'])
+
+
+# @app.route('/health')
+# def health_check():
+#     return "OK"
+
+
+@app.route('/chats/lasi')
+def ielasit_chatu():
+    with open("static/js/chat/chats.json", "r", encoding='utf-8') as f:
+        data = json.load(f)
+    return jsonify(data["messages"])
+
+
+@app.route('/chats/suuti', methods=["POST"])
+def suuti_zinju():
+    with open('static/js/chat/chats.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    message = request.json
+    message['timestamp'] = datetime.datetime.now().strftime("%d.%m.%Y %H:%M")
+    data["messages"].append(message)
+    data.update(data)
+    with open('static/js/chat/chats.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+    return ielasit_chatu()
+
+
 @app.errorhandler(404)
 def page_404(e):
     return render_template('404.html'), 404
@@ -57,7 +98,7 @@ def page_404(e):
 def index(page):
     return render_template('index.html',
                            csstime=os.path.getmtime("static/css/main.css"),
-                           nav=[routes['blackjack'], routes['countries']],
+                           nav=[routes['blackjack'], routes['countries'], routes['chat']],
                            page=page
                            )
 
---
+++ static/js/chat/Chat.js
@@ -0,0 +1,148 @@
+class Chat {
+    constructor(container) {
+        this.container = document.getElementById(container);
+        this.username = "NoName";
+        this.refresh = 50000;
+        this.usercolor = "";
+        this.usercolors = ["#FF4500", "#663399", "#4169E1", "#FF0000", "#2E8B57", "#EE82EE", "#008080", "#9ACD32", "#191970", "#800000", "#1E90FF", "#DC143C"];
+    }
+
+    start() {
+        //startDiv
+        let startDiv = document.createElement("div");
+        startDiv.id = "startDiv";
+        startDiv.className = "start-div";
+        this.container.appendChild(startDiv);
+        //user title
+        let userTitle = document.createElement("div");
+        userTitle.className = "user-title";
+        userTitle.innerHTML = "Lietotājvārds";
+        startDiv.appendChild(userTitle);
+        //input field
+        let user = document.createElement("input");
+        user.id = "username";
+        user.name = "username";
+        user.addEventListener("keyup", event => {
+            if (event.keyCode === 13) {
+                this.username = user.value;
+                this.usercolor = Math.floor(Math.random() * (this.usercolors.length + 1));
+                this.displayChat();
+            }
+        });
+        startDiv.appendChild(user);
+        //button
+        let startButton = document.createElement("div");
+        startButton.className = "user-login-button";
+        startButton.innerHTML = "Ieiet";
+        startButton.onclick = () => {
+            this.username = user.value;
+            this.usercolor = Math.floor(Math.random() * (this.usercolors.length + 1));
+            this.displayChat();
+        };
+        startDiv.appendChild(startButton);
+    }
+
+    displayChat() {
+        this.emptyDom(this.container);
+        //chatDiv
+        let chatDiv = document.createElement("div");
+        chatDiv.className = "chat-div";
+        this.container.appendChild(chatDiv);
+        //chat-output
+        let ul = document.createElement("ul");
+        ul.className = "chat-output";
+        ul.id = "chats";
+        chatDiv.appendChild(ul);
+        //chat-input
+        let inputDiv = document.createElement("div");
+        inputDiv.className = "chat-input";
+        chatDiv.appendChild(inputDiv);
+
+        //chat-input
+        let input = document.createElement("input");
+        input.id = "zinja";
+        input.placeholder = "Raksti ziņu";
+        input.addEventListener("keyup", event => {
+            if (event.keyCode === 13) {
+                this.suutiZinju().then(data => {
+                    this.raadiChataRindas(data);
+                });
+            }
+        });
+        inputDiv.appendChild(input);
+
+        let button = document.createElement("button");
+        button.title = "Sūtīt";
+        button.onclick = () => {
+            this.suutiZinju().then(data => {
+                this.raadiChataRindas(data);
+            });
+        };
+        inputDiv.appendChild(button);
+        this.lasiChatu();
+    }
+
+    async lasiChatu() {
+        const atbilde = await fetch('/chats/lasi');
+        const datuObjekts = await atbilde.json();
+        this.raadiChataRindas(datuObjekts);
+        await new Promise(resolve => setTimeout(resolve, this.refresh));
+        await this.lasiChatu();
+    }
+
+    async suutiZinju() {
+        let zinjasElement = document.getElementById("zinja");
+        let message = zinjasElement.value;
+        zinjasElement.value = "";
+        let response = await fetch('/chats/suuti', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                "message": message,
+                "user": this.username,
+                "timestamp": "",
+                "usercolor": this.usercolor
+            })
+        });
+        return response.json();
+    }
+
+
+    raadiChataRindas(dati) {
+        let chatUL = document.getElementById("chats");
+        // novaacam ieprieksheejo saturu
+        this.emptyDom(chatUL);
+        for (let rinda of dati) {
+            chatUL.appendChild(this.izveidoJaunuRindu(rinda));
+        }
+        // noskrolleejam uz leju pie peedeejaa chata texta
+        chatUL.scrollTop = chatUL.scrollHeight;
+    }
+
+
+    izveidoJaunuRindu(record) {
+        let li = document.createElement("li");
+        let div = document.createElement("div");
+        let user = document.createElement("div");
+        user.className = "user";
+        user.style.color = this.usercolors[record.usercolor];
+        user.innerHTML = record.user;
+        let timestamp = document.createElement("div");
+        timestamp.className = "timestamp";
+        timestamp.innerHTML = record.timestamp;
+        let text = document.createElement("div");
+        text.className = "text";
+        text.innerHTML = record.message;
+        li.appendChild(div);
+        div.appendChild(user);
+        div.appendChild(text);
+        div.appendChild(timestamp);
+        return li;
+    }
+
+    emptyDom(dom) {
+        while (dom.lastChild) {
+            dom.removeChild(dom.lastChild);
+        }
+    }
+}
